71523914fc40dbeb2e4033b408997b36f7a491d4
pyquint <83896843+pyquint@users.noreply.github.com>
2026-03-01 15:21:26 +0800

    small fixies

--- js/script.js
+++ js/script.js
@@ -1,20 +1,20 @@
 const observer = new IntersectionObserver((entries => {
     entries.forEach((entry) => {
-        if(entry.isIntersecting){
-            if(entry.target.classList.contains('text-container')){
-                entry.target.classList.add('show')
-            } else{
+        if (entry.isIntersecting) {
+            if (entry.target.classList.contains('text-anim')) {
+                entry.target.classList.add('show');
+            } else {
                 entry.target.classList.add('show');
             }
-        } else{
+        } else {
             entry.target.classList.remove("show");
         }
-    })
-}))
+    });
+}));
 
-const textContainers = document.querySelectorAll(".text-container");
+const textContainers = document.querySelectorAll(".text-anim");
 textContainers.forEach(textContainer => observer.observe(textContainer));
-const images = document.querySelectorAll(".image")
+const images = document.querySelectorAll(".img-anim");
 images.forEach(image => observer.observe(image));
 
 const cards = document.querySelectorAll(".observer-wrapper");
